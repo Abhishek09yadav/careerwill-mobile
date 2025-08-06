ef2cde136df829d8c996daf6f966f49bc274d437
Parv Vijayvargiya <parvvj2000@gmail.com>
2025-08-06 15:30:57 +0530

    test

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -121,6 +121,7 @@ class UserProvider extends ChangeNotifier {
       );
 
       if (kDebugMode) log("OTP Verify Response: ${response.data}");
+      log("message");
 
       if (response.statusCode == 200) {
         _message = response.data["message"];
